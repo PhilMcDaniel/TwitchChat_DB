d00d57fa084cbf65d571e5af6821b0d5f670a6cf
Phil McDaniel <mcdanipc@gmail.com>
2020-01-19 16:44:07 -0600

    Add project files.

---
+++ TwitchChat_DB/Chat_Message_F.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Chat_Message_F]
+(
+	[Chat_Message_Fact_Id] INT Identity(1,1) NOT NULL PRIMARY KEY
+	,[Message_DTM] datetime2 NOT NULL
+	,[Username] varchar(200) NOT NULL
+	,[Message_TXT] varchar(max) NULL
+)
