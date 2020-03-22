d476c151d50852391928bb476f1c15e272e253a1
Phil McDaniel <mcdanipc@gmail.com>
2020-03-22 09:05:35 -0500

    PCM - Add new column for channel name

--- TwitchChat_DB/Chat_Message_F.sql
+++ TwitchChat_DB/Chat_Message_F.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Chat_Message_F]
 (
 	[Chat_Message_Fact_Id] INT Identity(1,1) NOT NULL PRIMARY KEY
+	,[Channel_NME] varchar(200) NOT NULL
 	,[Message_DTM] datetime2 NOT NULL
 	,[Username] varchar(200) NOT NULL
 	,[Message_TXT] varchar(max) NULL
