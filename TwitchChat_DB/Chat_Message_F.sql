fc30244bc93b20814cf13de98271ecea402c3988
Phil McDaniel <mcdanipc@gmail.com>
2020-03-24 10:21:56 -0500

    PCM - add ETL_Batch_ID guid

--- TwitchChat_DB/Chat_Message_F.sql
+++ TwitchChat_DB/Chat_Message_F.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Chat_Message_F]
 (
 	[Chat_Message_Fact_Id] INT Identity(1,1) NOT NULL PRIMARY KEY
+	,ETL_Batch_ID char(36) NOT NULL
 	,[ETL_DataWritten_DTM] datetime2 DEFAULT GETDATE()
 	,[Channel_NME] varchar(200) NOT NULL
 	,[Message_DTE] date NOT NULL
